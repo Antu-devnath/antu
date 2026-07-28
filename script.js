fa35f12724e739bf394f1126d30ddf22cfda9ac7
Antu-devnath <antu3841@gmail.com>
2026-07-28 23:03:49 +0600

    Your commit message

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
 
-    let lastScrollTop = 0;
-    const header = document.querySelector('header');
+let lastScrollTop = 0;
+const header = document.querySelector('header');
 
+if (header) {
     window.addEventListener('scroll', function () {
-        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
         if (scrollTop > lastScrollTop) {
-            // Scrolling down
             header.classList.add('hidden');
         } else {
-            // Scrolling up
             header.classList.remove('hidden');
         }
-        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
     });
-    document.getElementById("toggle-projects").addEventListener("click", function () {
-        var extraProjects = document.querySelector(".extra-projects");
-        if (extraProjects.style.display === "none" || extraProjects.style.display === "") {
-            extraProjects.style.display = "grid"; // Change from block to grid
-            this.textContent = "Show Less";
-        } else {
-            extraProjects.style.display = "none";
-            this.textContent = "Show More";
-        }
-    });
-    
+}
